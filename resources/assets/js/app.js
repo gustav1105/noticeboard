50c3f3a69edc803b4b072f2a57450c334e1c758d
Gustav Coetzee <gustav1105@gmail.com>
2019-05-01 19:27:15 +0200

    jwt authentication created - auth/login

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,19 @@
+require('./bootstrap')
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import VeeValidate from 'vee-validate'
+import routes from './routes.js'
+import storeData from './store.js'
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+Vue.use(VeeValidate);
+Vue.use(VueRouter);
+Vue.use(Vuex);
 
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+const store = new Vuex.Store(storeData);
 
 const app = new Vue({
-    el: '#app'
-});
+    el: '#app',
+    router: new VueRouter(routes),
+    store,
+})
---
+++ resources/assets/js/routes.js
@@ -0,0 +1,10 @@
+import Home from './components/Home'
+
+export default {
+    routes: [
+        {
+            path: '/',
+            component: Home
+        }
+    ]
+}
